5fd17f5833957855284ae0b95a3754d12bc8ede3
Katerina Karlovska <karlovska.kat@gmail.com>
2023-01-30 15:05:55 +0100

    snaha o ukol

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-// tady je místo pro náš program
+// tady je místo pro náš program//
+
+// barva odstavce//
+function zmenBarvuOdstavce (){
+let odstavec = document.querySelector("#odstavec");
+odstavec.classList.toggle("stylodstavce");
+}
+
+// velikost písma odstavce//
+
+
+
+// přehrávač//
+
+
+let prehravac;
+prehravac = document.querySelector (".prehravac");
+
+function zapniHudbu(){
+prehravac.play();
+}
+
+function vypniHudbu(){
+prehravac.pause();
+}
+
+function hrajOdZacatk(){
+prehravac.currentTime = 0;
+}
+
+function mute (){
+prehravac.mute();
+}
+  
+    
+function pridejHlasitost (){
+prehravac.volume(0.5);
+}
+
+let tlacitko = document.querySelector(".play");
+.addEventListener "click", zavolejUdalost(event)
+
+function zavolejUdalost (udalost) {
+    let prvek = udalost.target;
+    prvektext.log(udalost)
+    console.log(udalost)
